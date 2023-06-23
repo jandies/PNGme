6a243ab03b0eed330d1fc3a31a6cf9ffb8d31ddb
Luke Falvey <lukebfalvey@gmail.com>
2023-06-23 22:04:16 +1200

    Add a CLI

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,24 @@ mod chunk_type;
 mod commands;
 mod png;
 
+use crate::args::Cli;
+use clap::Parser;
 use std::fs;
 
 fn main() {
-
-    // Obtain PNG as Vec<u8>
-    let file_path = "image.png";
-    let file_as_bytes1 = fs::read(file_path).expect("Failed to read file");
-
-    // Encode PNG with test message
-    let message = "This is our secrete message".to_owned();
-    let chunk_t = "RuSt".to_owned();
-
+    let args = Cli::parse();
+    let file_as_bytes1 = fs::read(&args.png_path).expect("Failed to read file");
     println!("PNG PRE: {:?}\n\n\n", &file_as_bytes1);
-    let encoded_png = commands::encode(&file_as_bytes1, chunk_t, message).unwrap();
-    fs::write(file_path, encoded_png.as_bytes()).expect("Failed to write");
-    let file_as_bytes2 = fs::read(file_path).expect("Failed to read file");
+    let encoded_png = commands::encode(&file_as_bytes1, args.chunk_type, args.message).unwrap();
+    fs::write(&args.png_path, encoded_png.as_bytes()).expect("Failed to write");
+    let file_as_bytes2 = fs::read(&args.png_path).expect("Failed to read file");
 
     println!("PNG POST: {:?}\n\n\n", &file_as_bytes2);
     println!("{:?}", file_as_bytes1 == file_as_bytes2);
     // Decode PNG with test message
-    let chunk_t2 = "RuSt".to_owned();
+    let chunk_t2: String = "RuSt".to_owned();
     match commands::decode(&file_as_bytes2, chunk_t2) {
         Ok(_) => println!("Decode successful\n\n\n"),
-        Err(e) => eprintln!("{}", e)
+        Err(e) => eprintln!("{}", e),
     }
 }
